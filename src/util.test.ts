6031cf8141fdf5029f78372a17c2596bcb88e2d8
lazos <lazos@lazos.me>
2022-09-13 21:33:06 +0200

    tests

---
+++ src/util.test.ts
@@ -0,0 +1,25 @@
+import {asCancelablePromise, asPromise} from "./util";
+
+describe('testing utils', () => {
+    test('callback as promise', async () => {
+        const r = await asPromise(() => {
+            return '_test'
+        })
+        expect(r).toBe('_test')
+    })
+    test('cancellable promise', async () => {
+        const p = new Promise((resolve, reject) => {
+            setTimeout(() => resolve(true), 100)
+        })
+        let erred = false
+        const c = asCancelablePromise(p)
+        c.cancel()
+        try {
+            const res = await c.promise
+        } catch (err: any) {
+            erred = true
+            expect(err.isCanceled).toBe(true)
+        }
+        expect(erred).toBe(true)
+    })
+})
